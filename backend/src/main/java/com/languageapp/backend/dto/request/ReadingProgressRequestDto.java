ed57351502a6ad9b1b0d610f40738a60f2b662db
SKharel10 <sagarkharel001@gmail.com>
2026-08-27 23:55:01 +0100

    fix: update reading progress request dto pageId field to be NotNull.

--- backend/src/main/java/com/languageapp/backend/dto/request/ReadingProgressRequestDto.java
+++ backend/src/main/java/com/languageapp/backend/dto/request/ReadingProgressRequestDto.java
@@ -1,5 +1,7 @@
 package com.languageapp.backend.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record ReadingProgressRequestDto(UUID bookId, UUID pageId) {}
+public record ReadingProgressRequestDto(UUID bookId, @NotNull UUID pageId) {}
