78ccdb44713ffb7facfedd9d0288b04b21fa4acd
SKharel10 <sagarkharel001@gmail.com>
2026-08-28 19:44:43 +0100

    format: apply spotless

--- backend/src/main/java/com/languageapp/backend/dto/request/UserRequestDto.java
+++ backend/src/main/java/com/languageapp/backend/dto/request/UserRequestDto.java
@@ -3,5 +3,4 @@ package com.languageapp.backend.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UserRequestDto(
-        @NotBlank @Size(min = 2, max = 70) String name) {}
+public record UserRequestDto(@NotBlank @Size(min = 2, max = 70) String name) {}
